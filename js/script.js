f0a133d1ef6f3b8d2015b6abc8ec62a9e11cfe76
Woko David <wokodavid001@gmail.com>
2021-12-05 15:08:56 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// var q;
+// q=34;
+// if (q==undefined){console.log("fuckers");}
+// else{console.log("fuck");}
+// // lecture 41
+// var string ="my name is";
+// string +=" DAVIDO";
+// console.log(string);
+// console.log(21/5);
+// var a= 40, b= 20;
+// if (a==b){console.log("equal")};
+
+// var a='40', b=40
+// if (a===b){console.log("equal");}
+// else{console.log("im a bad motherfucker")}
+// // lecture 42
+// if(null||false||""||undefined){console.log("won't execute");}
+// else{console.log("execute")}
+// // lecture 43 (for loop)
+// var sum=0
+// for(var i=0; i<10; i++ )
+// console.log(i);
+// {sum+=i};
+// console.log(sum)
+// {console.log("the value of sum through 9 =" + sum);}
+// // lecture 44 setting default values
+// function orderchickenwith(sidedish){sidedish= sidedish||"riced"
+// console.log(sidedish)}
+
+// orderchickenwith();
+// lecture 45 creating objects
+/**var company = new Object();
+company.name= "facebook";
+console.log(company)
+company.ceo= new Object();
+company.ceo.Firstname="Mark"
+company.ceo.favcolor="blue"**/
+// creating objects easier way
+var company={name:"Facebook",
+ceo:{firstname:"Mark", favcolor:"blue"},stock:120};
+console.log(company)
+
+
